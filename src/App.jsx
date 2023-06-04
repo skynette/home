12741d5c207e25d80e49091d65f8122779de326b
Bukola-O <120281590+Bukola-O@users.noreply.github.com>
2023-06-03 19:32:32 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <div className=" text-red-500 text-3xl font-bold underline">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LogIn/>}/>
+        <Route path="/Sign-up" element={<SignUp/>}/>
+        <Route path="/Home/:id" element={<Home/>}/>
+        <Route path="/Search/:searchTerm" element={<SearchFeed/>}/>
+        <Route path="/User/:id" element={<UserProfile/>}/>
+        <Route path="/User/:id/upload" element={<Upload/>}/>
+        <Route path="/House/:id" element={<HouseDetail/>}/>
+        <Route path="/Profile/:id" element={<ProfileDetail/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
