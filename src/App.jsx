bc90839e08b806b35086919b4113b8079d2f9666
Joshua <cutejosh2@gmail.com>
2023-06-05 23:02:27 +0100

    basic structure for scenes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Register from './scenes/Register';
+import Login from './scenes/Login';
+import Home from './scenes/Home';
+import Profile from './scenes/Profile';
 
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LogIn/>}/>
-        <Route path="/Sign-up" element={<SignUp/>}/>
-        <Route path="/Home/:id" element={<Home/>}/>
-        <Route path="/Search/:searchTerm" element={<SearchFeed/>}/>
-        <Route path="/User/:id" element={<UserProfile/>}/>
-        <Route path="/User/:id/upload" element={<Upload/>}/>
-        <Route path="/House/:id" element={<HouseDetail/>}/>
-        <Route path="/Profile/:id" element={<ProfileDetail/>}/>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/home/:id" element={<Home/>}/>
+        <Route path="/search/:searchTerm" element={<SearchFeed/>}/>
+        <Route path="/user/:id" element={<Profile/>}/>
+        <Route path="/user/:id/upload" element={<Upload/>}/>
+        <Route path="/house/:id" element={<HouseDetail/>}/>
+        <Route path="/profile/:id" element={<ProfileDetail/>}/>
+        <Route path="/user/:id/edit" element={<EditProfile/>}/>
       </Routes>
     </BrowserRouter>
     
