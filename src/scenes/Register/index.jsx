4ba6afabbd53f4ce8c04cc9871d0a72facc4664c
Bukola-O <120281590+Bukola-O@users.noreply.github.com>
2023-06-07 19:39:53 -0600

    navigation

--- src/scenes/Register/index.jsx
+++ src/scenes/Register/index.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
+
+  let navigate = useNavigate();
+
+  const handleClick= ()=>{
+      navigate("/")
+  }
   return (
     <div className='bg-body-400 grid justify-center items-center h-screen w-screen font-main'> 
       <div className='bg-body-300 w-[100vw] h-[100vh] shadow-md rounded-lg sm:w-[65vw] sm:h-auto lg:w-[60vw]'>
         <div className='flex w-[100%] justify-center border-b border-body-400 mt-2 mr-2'>
-          <div className='ml-2 w-[50%] text-center px-2 py-6 bg-body-400 hover:cursor-pointer font-bold mr-2'>Login</div>
+          <div className='ml-2 w-[50%] text-center px-2 py-6 bg-body-400 hover:cursor-pointer font-bold mr-2' onClick={handleClick}>Login</div>
           <div className='ml-2 w-[50%] text-center px-2 py-6  hover:cursor-pointer font-bold' >Register</div>
         </div>
         <div className='flex justify-between'>
