a07bd19c6d4fe958dc3733ff7a732f71d5e3d054
Bukola-O <120281590+Bukola-O@users.noreply.github.com>
2023-06-14 17:57:23 -0600

    Cities

--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -17,6 +17,55 @@ const Home = () => {
                 </div>
 
             </div>
+            <div className="bg-body-300">
+                <div className="text-center pt-12">
+                    <h2 className="text-body-800 font-bold text-3xl pt-12"> Find Properties in These Cities</h2>
+                    <p className="text-body-500 mt-2">Handpicked properties by our team</p>
+                </div>
+                <div className="px-6 py-12 lg:px-40 lg:py-20">
+                    <div className="block lg:grid lg:grid-cols-3 lg:gap-8">
+                        <div className=" h-[60vh] lg:h-[40vh] overflow-hidden lg:col-span-1 rounded-lg relative">
+                            <div className="h-[100%] w-[100%] bg-[url('src/assets/Cities/ben-o-bro-wpU4veNGnHg-unsplash.jpg')] bg-cover bg-center rounded-lg hover:transform hover:scale-105 hover:rotate-1 transition-transform ease-in-out duration-300">
+
+                            </div>
+                            <div className="absolute bottom-8 text-center left-[38%]">
+                                <h2 className="text-body-300 text-lg font-bold">Miami</h2>
+                                <p className="text-body-300 font-light">24 properties</p>
+                            </div>
+                        </div>
+                        <div className=" mt-4 h-[40vh] overflow-hidden lg:col-span-2 lg:mt-0 rounded-lg relative">
+                            <div className="h-[100%] w-[100%] bg-[url('src/assets/Cities/clement-falize-9uNFBOXBEBc-unsplash.jpg')] bg-cover bg-center rounded-lg hover:transform hover:scale-105 hover:rotate-1 transition-transform ease-in-out duration-300">
+
+                            </div>
+                            <div className="absolute bottom-8 text-center left-[38%]">
+                                <h2 className="text-body-300 text-lg font-bold">Los Angeles</h2>
+                                <p className="text-body-300 font-light">18 properties</p>
+                            </div>
+                        </div>
+                        <div className="mt-4 h-[40vh] overflow-hidden lg:col-span-2 lg:mt-0 rounded-lg relative">
+                            <div className="h-[100%] w-[100%] bg-[url('src/assets/Cities/ling-tang-XHLKlSppBvk-unsplash.jpg')] bg-cover bg-center rounded-lg hover:transform hover:scale-105 hover:rotate-1 transition-transform ease-in-out duration-300">
+
+                            </div>
+                            <div className="absolute bottom-8 text-center left-[38%]">
+                                <h2 className="text-body-300 text-lg font-bold">New York</h2>
+                                <p className="text-body-300 font-light">89 properties</p>
+                            </div>
+                        </div>
+                        <div className="mt-4 h-[60vh] lg:h-[40vh] overflow-hidden lg:col-span-1 lg:mt-0 rounded-lg relative">
+                            <div className="h-[100%] w-[100%] bg-[url('src/assets/Cities/pedro-lastra-Nyvq2juw4_o-unsplash.jpg')] bg-cover bg-center rounded-lg hover:transform hover:scale-105 hover:rotate-1 transition-transform ease-in-out duration-300">
+
+                            </div>
+                            <div className="absolute bottom-8 text-center left-[38%]">
+                                <h2 className="text-body-300 text-lg font-bold">Florida</h2>
+                                <p className="text-body-300 font-light">47 properties</p>
+                            </div>
+                        </div>
+                        
+
+                    </div>
+                </div>
+
+            </div>
 
             <h1>Home</h1>
         </div>
