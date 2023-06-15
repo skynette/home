39d5b0b21d778fac92a7ee6bffddda69d504ef28
Joshua <cutejosh2@gmail.com>
2023-06-15 06:07:32 +0100

    mobile nav

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { FaUserCircle } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
@@ -8,9 +10,10 @@ const Navbar = () => {
 	const loginNav = () => {
 		navigate("/login");
 	};
+	const [isNavOpen, setIsNavOpen] = useState(false);
 
 	return (
-		<div className="border-b border-body-500">
+		<div className="border-b border-body-500 relative">
 			<div className="w-5/6 mx-auto">
 				<nav className={`h-16 ${flexBetween}`}>
 					{/* left side */}
@@ -39,7 +42,36 @@ const Navbar = () => {
 					</div>
 
 					<div className="hover:cursor-pointer md:hidden">
-						<AiOutlineMenu className="text-white h-7 w-7" />
+						{!isNavOpen && (
+							<AiOutlineMenu
+								className="text-white h-7 w-7"
+								onClick={() => setIsNavOpen(!isNavOpen)}
+							/>
+						)}
+					</div>
+
+					<div
+						className={`fixed top-0 left-0 z-[20] w-3/4 h-[100vh] bg-white transform transition-transform duration-300 ${
+							isNavOpen ? "translate-x-0" : "-translate-x-full"
+						} nav-menu`}
+					>
+						{/* Navigation menu content */}
+						<div className="flex justify-between items-center h-16 px-4 border-b">
+							<h1 className="text-2xl text-cred-500">Home</h1>
+							<MdClose
+								className="text-black h-7 w-7"
+								onClick={() => setIsNavOpen(!isNavOpen)}
+							/>
+						</div>
+            <div className="bg-body-300 hover:cursor-pointer">
+              <ul className="flex flex-col justify-center items-start gap-4">
+                <li className="text-body-800 py-2 px-4 border-b w-full">About</li>
+                <li className="text-body-800 py-2 px-4 border-b w-full">Contact</li>
+                <li className="text-body-800 py-2 px-4 border-b w-full" onClick={loginNav}>
+                  Login
+                </li>
+              </ul>
+            </div>
 					</div>
 				</nav>
 			</div>
