ae73a09005690fb2683a059c4e8f1d44dd4cd526
Bukola-O <120281590+Bukola-O@users.noreply.github.com>
2023-06-24 01:51:07 -0600

    Update index.jsx

--- src/components/CardV3/index.jsx
+++ src/components/CardV3/index.jsx
@@ -13,8 +13,8 @@ const CardV3 = (props) => {
         <div className='flex w-[80%] gap-3'>
             <img className="w-[30%] h-[10vh] object-center object-cover rounded-md" src={img} />
             <div>
-                <h3 className='font-bold text-body-800 text-lg'>{name}</h3>
-                <h3 className="font-bold text-cred-500 text-lg">
+                <h3 className='font-semibold text-body-800 text-lg'>{name}</h3>
+                <h3 className="font-semibold text-cred-500 text-lg">
                     {price}/<span className="text-md">mo</span>
                 </h3>
                 <div className='flex justify-between'>
