f9da30a2c6c5418ac106e7262593a4f2d29a4ba3
Bukola-O <120281590+Bukola-O@users.noreply.github.com>
2023-06-24 02:14:51 -0600

    Update index.jsx

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,18 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from '../Card';
 import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai'
 
 const Pagination = (props) => {
-    
-    const {data} = props;
 
-  const itemsPerPage = 10; 
-  const totalItems = data.length; 
-  const totalPages = Math.ceil(totalItems / itemsPerPage); 
+  const { data } = props;
 
-  const [currentPage, setCurrentPage] = useState(1); 
+  const itemsPerPage = 10;
+  const totalItems = data.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
 
-  
+  const [currentPage, setCurrentPage] = useState(1);
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, [currentPage])
+
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
@@ -20,57 +25,57 @@ const Pagination = (props) => {
   const startIndex = (currentPage - 1) * itemsPerPage;
   const endIndex = startIndex + itemsPerPage;
 
-  
-  const itemsToDisplay =  data.slice(startIndex, endIndex);
+
+  const itemsToDisplay = data.slice(startIndex, endIndex);
 
   return (
     <div >
-        <div className='grid grid-cols-1 md:grid-cols-2 gap-0'>
-            {itemsToDisplay.map((card, j) => (
-            <Card 
-                key={j}
-                type={card.type}
-                price={card.price}
-                img={card.img}
-                name={card.name}
-                location={card.location}
-                beds={card.beds}
-                baths={card.bath}
-                sqft={card.sqft}
-                profileimg={card.profileImg}
-                profilename={card.profileName}
-                date={card.date}
-                mode={card.mode}
-            />
-            ))}
-        </div>
-      
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-0'>
+        {itemsToDisplay.map((card, j) => (
+          <Card
+            key={j}
+            type={card.type}
+            price={card.price}
+            img={card.img}
+            name={card.name}
+            location={card.location}
+            beds={card.beds}
+            baths={card.bath}
+            sqft={card.sqft}
+            profileimg={card.profileImg}
+            profilename={card.profileName}
+            date={card.date}
+            mode={card.mode}
+          />
+        ))}
+      </div>
+
       <div className='flex justify-center pb-10'>
         <div className='flex items-center'>
-            <button className='p-3 rounded-full bg-body-300 mr-2 text-body-600 hover:bg-cred-500 hover:text-body-300 border border-body-500'
+          <button className='p-3 rounded-full bg-body-300 mr-2 text-body-600 hover:bg-cred-500 hover:text-body-300 border border-body-500'
             disabled={currentPage === 1}
-             onClick={() => handlePageChange(currentPage - 1)}
-            >
-            <AiFillCaretLeft/>
-            </button>
+            onClick={() => handlePageChange(currentPage - 1)}
+          >
+            <AiFillCaretLeft />
+          </button>
 
-       
-            {Array.from({ length: totalPages }, (_, index) => (
-            <button className={`h-[40px] w-[40px] rounded-full mr-2 hover:bg-cred-500 hover:text-body-300 border border-body-500 ${(currentPage===index+1)?'text-body-300 bg-cred-500':'bg-body-300 text-body-600'}`}
-                key={index + 1}
-                onClick={() => handlePageChange(index + 1)}
-                disabled={currentPage === index + 1}
-            >
-                {index + 1}
-            </button>
-            ))}
 
-            <button className='p-3 rounded-full bg-body-300 text-body-600 hover:bg-cred-500 hover:text-body-300 border border-body-500' 
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button className={`h-[40px] w-[40px] rounded-full mr-2 hover:bg-cred-500 hover:text-body-300 border border-body-500 ${(currentPage === index + 1) ? 'text-body-300 bg-cred-500' : 'bg-body-300 text-body-600'}`}
+              key={index + 1}
+              onClick={() => handlePageChange(index + 1)}
+              disabled={currentPage === index + 1}
+            >
+              {index + 1}
+            </button>
+          ))}
+
+          <button className='p-3 rounded-full bg-body-300 text-body-600 hover:bg-cred-500 hover:text-body-300 border border-body-500'
             disabled={currentPage === totalPages}
             onClick={() => handlePageChange(currentPage + 1)}
-         >
-            <AiFillCaretRight/>
-            </button>
+          >
+            <AiFillCaretRight />
+          </button>
         </div>
       </div>
     </div>
