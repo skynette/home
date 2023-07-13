c8eeb35235c47cdcf5388e6576a984a89f23e008
Bukola-O <120281590+Bukola-O@users.noreply.github.com>
2023-07-12 22:35:38 -0600

    Update index.jsx

--- src/scenes/Home/index.jsx
+++ src/scenes/Home/index.jsx
@@ -81,7 +81,7 @@ const Home = () => {
                     <h2 className="text-body-800 font-bold text-3xl pt-10">Why Choose Us</h2>
                     <p className="text-body-500 mt-2">We provide full service at every step.</p>
                 </div>
-                <div className="md:mx-[20vw] mx-4 my-10 lg:flex lg:gap-6" >
+                <div className="md:mx-[15vw] mx-4 my-10 lg:flex lg:gap-6" >
                     <div className="bg-body-300 text-center rounded-lg flex flex-col items-center mb-5 shadow-md hover:shadow-lg transform hover:scale-[1.02] hover:transition-transform hover:duration-500 city">
                         <div className="my-11">
                             <GiHighFive className="p-6 rounded-full bg-cred-200 h-[130px] w-[130px] text-cred-500 cicon" />
