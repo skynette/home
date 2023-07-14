ee5dfe626203983893db21119d758ad71b49749f
Bukola-O <120281590+Bukola-O@users.noreply.github.com>
2023-07-14 15:30:24 -0600

    Update index.jsx

--- src/components/Upload/index.jsx
+++ src/components/Upload/index.jsx
@@ -11,14 +11,14 @@ const Upload = () => {
       <div className='bg-body-300 p-6 pb-14 rounded-md'>
         <h3 className='text-2xl font-semibold text-body-800 my-6'>Create Listing</h3>
         <label className='block text-body-800 my-2' htmlFor='title'>Property Title</label>
-        <input className='w-full h-12 rounded-md outline-none px-4 text-body-800' id='title' name='title' type='text' />
+        <input className='w-full h-12 rounded-md outline-none px-4 text-body-800 border border-body-400' id='title' name='title' type='text' />
         <label className='block text-body-800 my-2 mt-6' htmlFor='description'>Description</label>
-        <textarea id='description' name='description' className='w-full h-40 rounded-md outline-none px-4 pt-4' />
+        <textarea id='description' name='description' className='w-full h-40 rounded-md outline-none px-4 pt-4 border border-body-400' />
         <div className='lg:flex lg:gap-6'>
           <div className='mt-6 w-full'>
             <label className='block text-body-800 my-2' htmlFor='type'>Type</label>
             <div className='relative'>
-              <select id='type' name='type' className='w-full h-12 rounded-md outline-none px-4 text-body-800'>
+              <select id='type' name='type' className='w-full h-12 rounded-md outline-none px-4 text-body-800 border border-body-400'>
                 <option value="Type1">Type1</option>
                 <option value="Type2">Type2</option>
                 <option value="Type3">Type3</option>
@@ -31,7 +31,7 @@ const Upload = () => {
           <div className='mt-6 w-full'>
             <label className='block text-body-800 my-2' htmlFor='status'>Status</label>
             <div className='relative'>
-              <select id='status' name='status' className='w-full h-12 rounded-md outline-none px-4 text-body-800 '>
+              <select id='status' name='status' className='w-full h-12 rounded-md outline-none px-4 text-body-800 border border-body-400'>
                 <option value="Status1">Status1</option>
                 <option value="Status2">Status2</option>
                 <option value="Status3">Status3</option>
@@ -45,15 +45,15 @@ const Upload = () => {
         <div className='lg:flex lg:gap-4'>
           <div className='mt-6 w-full'>
             <label className='block text-body-800 my-2' htmlFor='price'>Price</label>
-            <input id='price' type='text' className='w-full h-12 rounded-md outline-none px-4 text-body-800'/>
+            <input id='price' type='text' className='w-full h-12 rounded-md outline-none px-4 text-body-800 border border-body-400'/>
           </div>
           <div className='mt-6 w-full'>
             <label className='block text-body-800 my-2' htmlFor='area'>Area</label>
-            <input id='area' type='text' className='w-full h-12 rounded-md outline-none px-4 text-body-800'/>
+            <input id='area' type='text' className='w-full h-12 rounded-md outline-none px-4 text-body-800 border border-body-400'/>
           </div>
           <div className='mt-6 w-full'>
             <label className='block text-body-800 my-2' htmlFor='rooms'>Rooms</label>
-            <input id='rooms' type='number' className='w-full h-12 rounded-md outline-none px-4 text-body-800'/>
+            <input id='rooms' type='number' className='w-full h-12 rounded-md outline-none px-4 text-body-800 border border-body-400'/>
           </div>
         </div>
       </div>
